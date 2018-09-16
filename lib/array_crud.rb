eac54b239dee9e4dad0c76f1bc9369dba8380785
Will Figueiredo <will@wfigueiredo.com>
2018-09-16 00:10:26 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,36 +3,33 @@ def create_an_empty_array
 end
 
 def create_an_array
-  array = [1. 2. 3. 4]
+  scoringLeaders = ["Ovechkin", "Stamkos", "Giroux", "McDavid"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array = array.push(element)
 
 end
 
 def add_element_to_start_of_array(array, element)
-    array = array.unshift(element)
 
 end
 
 def remove_element_from_end_of_array(array)
-  array.pop
+
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift
 
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[index_number]
+
 end
 
 def retrieve_first_element_from_array(array)
-  return array[0]
+
 end
 
 def retrieve_last_element_from_array(array)
-  return array[-1]
+
 end
