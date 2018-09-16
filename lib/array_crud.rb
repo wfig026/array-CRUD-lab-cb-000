254a7bcc322ea36595faf9fbb610a362747f79fb
Will Figueiredo <will@wfigueiredo.com>
2018-09-16 00:00:04 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-
+  scoringLeaders = ["Ovechkin", "Stamkos", "Giroux", "McDavid"]
+  scoringLeaders.push["Vorachek"]
 end
 
 def add_element_to_start_of_array(array, element)
