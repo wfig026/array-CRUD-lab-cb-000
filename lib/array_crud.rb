c64dce340c21b2be2596a54824bd3bff3db4a35e
Will Figueiredo <will@wfigueiredo.com>
2018-09-16 00:08:26 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,10 +19,10 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.shift
+  # array.shift
 
 def retrieve_element_from_index(array, index_number)
-  array[index_number]
+  # array[index_number]
 end
 
 def retrieve_first_element_from_array(array)
